4338e010f5a2c38515816c4dfa33ba991ecf9d34
shazebkhan22 <shazeb.khan@somaiya.edu>
2025-02-05 18:46:11 +0530

    Removed spaces

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,4 @@
 
-
-
-
 const Map = () => {
   const mapUrl = " https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.004698718957!2d72.8922616!3d19.1074498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c8736f8ffed5%3A0xa3a05dbc1da26e17!2sHimalaya%20Plast!5e0!3m2!1sen!2sin!4v1738518321347!5m2!1sen!2sin"
 
